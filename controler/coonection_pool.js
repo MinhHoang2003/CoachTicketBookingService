6b9de17f611c4c9c68e93ffd217e0a6e03b17798
hoangpm <hoangd16ptit@gmail.com>
2021-01-17 22:45:10 +0700

    Change config pool

--- controler/coonection_pool.js
+++ controler/coonection_pool.js
@@ -1,20 +1,25 @@
 const util = require('util')
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: '203.171.21.114',
     user: 'project',
     password: 'Trinhhoainam1998@',
-    database: 'coachticketbookingdb'
+    database: 'coachticketbookingdb',
+    waitForConnections: true,
+    queueLimit: 0
 })
 
 pool.getConnection((err, connection) => {
     if (err) {
         console.log('Something went wrong connecting to the databse :', err)
     }
-    if (connection) connection.release();
+    if (connection) {
+        
+        connection.release()
+    };
     return;
 })
 
